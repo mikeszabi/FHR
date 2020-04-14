47ac601ebff12907f2fbc467e0c61eb1c40004d4
mikeszabi <szabolcs.mike@gmail.com>
2020-04-14 16:46:12 +0200

    sequencial

--- fhr2.py
+++ fhr2.py
@@ -6,9 +6,10 @@ Created on Tue Apr 14 12:20:36 2020
 """
 
 import os
-import glob
 import numpy as np
 import pandas as pd
+
+from tools.file_helper import filelist_in_depth 
 
 from matplotlib import pyplot as plt
 
@@ -17,15 +18,19 @@ N=1200
 T=0.003 # sec
 
 base_dir=r'D:\DATA\FHR'
-data_dir=os.path.join(base_dir,'Zoli-20200327T075837Z-001','Zoli','0')
+data_dir=os.path.join(base_dir,'Zoli','1')
 
-measure_data_files=glob.glob(os.path.join(data_dir,'*.npy'))
+measure_data_files=filelist_in_depth(base_dir,level=2,date_sort=False,included_extenstions = ['*.npy'])
 
 
 for i_file in range(len(measure_data_files)):
-    i_file=1
+    i_file=0
     
     measure_data=np.load(measure_data_files[i_file])
+    
+    fig, ax = plt.subplots(2,1)
+    ax[0].plot(measure_data[:,1200])
+    ax[1].plot(measure_data[:,1201])
     
     mdf=pd.DataFrame(measure_data[:,:1200].copy())
     roll_mdf=mdf.rolling(axis=1,window=20,center=True)
@@ -37,9 +42,9 @@ for i_file in range(len(measure_data_files)):
     
     for i_sample in range(mdf.shape[0]):
         
-        
-        plt.plot(mdf.iloc[i_sample,:N])
-        plt.plot(rolling_max.iloc[i_sample,:N])
+        fig, ax = plt.subplots()
+        ax.plot(mdf.iloc[i_sample,:N])
+        ax.plot(rolling_max.iloc[i_sample,:N])
         
         fhr=measure_data[i_sample,N]
         print(fhr)
@@ -54,4 +59,5 @@ for i_file in range(len(measure_data_files)):
         ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
         ax.text(0.8,0.8,str(fhr),horizontalalignment='center',verticalalignment='center', transform=ax.transAxes)
         ax.set_ylim(0,0.05)
+        ax.set_xlim(0,1.0/(2.0*T))
         plt.show()
